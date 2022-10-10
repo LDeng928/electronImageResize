55e85bcea8bba6f2682e265e110dc52b7970f9ad
Ldeng928 <lideng928@gmail.com>
2022-10-10 13:01:00 -0400

    app initial dev

---
+++ renderer/js/renderer.js
@@ -0,0 +1,99 @@
+const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
+
+function loadImage(e) {
+  const file = e.target.files[0]; // first and only item, which is the image
+
+  if (!isFileImage(file)) {
+    alertError('Please select an image.');
+    return;
+  }
+
+  // Get original dimensions
+  // Image constructor: https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
+  const image = new Image();
+  // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+  image.src = URL.createObjectURL(file);
+
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
+  form.style.display = 'block';
+  filename.innerText = file.name;
+  outputPath.innerText = path.join(os.homedir(), 'imageResizer');
+}
+
+// Send image data to main
+function sendImage(e) {
+  e.preventDefault();
+
+  const width = widthInput.value;
+  const height = heightInput.value;
+  const imgPath = img.files[0].path;
+
+  if (!img.files[0]) {
+    alertError('Please upload an image');
+    return;
+  }
+
+  if (width === '' || height === '') {
+    alertError('Please fill in a height and width');
+    return;
+  }
+
+  // Send to main using ipcRenderer
+  ipcRenderer.send('image:resize', {
+    imgPath,
+    width,
+    height,
+  });
+}
+
+// Catch the image:done event
+ipcRenderer.on('image:done', () => {
+  alertSuccess(`Image resized to ${widthInput.value} and ${heightInput.value}`);
+});
+
+// Make sure file is an image
+function isFileImage(file) {
+  const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
+
+  return file && acceptedImageTypes.includes(file['type']);
+}
+
+// Alert error
+function alertError(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: 'red',
+      color: 'white',
+      textAlign: 'center',
+    },
+  });
+}
+
+// Success message
+function alertSuccess(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: 'green',
+      color: 'white',
+      textAlign: 'center',
+    },
+  });
+}
+
+img.addEventListener('change', loadImage);
+form.addEventListener('submit', sendImage);
